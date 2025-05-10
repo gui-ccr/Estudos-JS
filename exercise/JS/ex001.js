e8ea072ae6c68dfaed179299a6e908fd5033e5b4
Guilherme <116519824+gui-ccr@users.noreply.github.com>
2025-05-10 18:04:53 -0300

    aprendendo a usar JS

---
+++ exercise/JS/ex001.js
@@ -0,0 +1,6 @@
+window.alert('Minha primeira mensagem em JS') // exibe um alerta na tela
+window.confirm('Esta gostando de JS?') // emite uma janela com botao 'ok' e 'cancel'
+window.prompt('Qual seu nome?') //exibe uma janela que o usuario pode entrar com algum dado
+
+// ou então posso omitir o window e deixar apenas alert
+// JS não precisa colocar ;
