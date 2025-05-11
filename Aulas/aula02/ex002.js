76e7e6decded30ba53febbbf9ffa1ea9dd2c22a0
Guilherme <116519824+gui-ccr@users.noreply.github.com>
2025-05-11 10:38:12 -0300

    up

---
+++ Aulas/aula02/ex002.js
@@ -0,0 +1,2 @@
+var nome = window.prompt('Qual seu nome') // a variavel nome vai armazenar o dado de entrada do usuario 
+alert('olha ' + nome + ' prazer em te conhecer')
--- exercise/JS/ex002.js
+++
@@ -1,7 +0,0 @@
-// em java script podemos definir variaveis como var ou let
-
-var carro = "carro1"
-let moto = "moto1"
-var numero = 123
-var numero_ponto_flutuante = 2123.2313
-let boleano = true 
